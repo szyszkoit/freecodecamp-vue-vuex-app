1595f8f6c429c50d3fa42d27bf78e451f57eb3b6
Krzysztof Szyszko <k.szyszko14@gmail.com>
2020-04-06 21:45:26 +0200

    isolating pet table to individual component

--- src/views/Dogs.vue
+++ src/views/Dogs.vue
@@ -1,19 +1,20 @@
 <template>
   <div>
-    <h1>Dogs for adoption</h1>
-    <b-table striped hover :items="dogs">
-      <template v-slot:cell(name)="data">
-        <!-- `data.value` is the value after formatted by the Formatter -->
-        <router-link :to="`/pets/dogs/${data.index}`">{{ data.value }}</router-link>
-      </template>
-    </b-table>
+    <PetTable
+      species="dogs"
+      :pets="dogs"
+    />
   </div>
 </template>
 
 <script>
 import { mapState } from 'vuex'
+import PetTable from '@/components/PetTable.vue'
 
 export default {
+  components: {
+    PetTable
+  },
   data () {
     return {}
   },
